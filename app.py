b8fd4b509f8e52d89534b7b5ebd98d59ea4ec3b5
Mirkar, Shahezad <Shahezad_Mirkar@bmc.com>
2025-08-10 10:49:59 +0530

    Code updated CI/CD should trigger Now

--- app.py
+++ app.py
@@ -75,6 +75,6 @@ def predict():
 @app.route("/metrics")
 def metrics():
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
-#This is the main Function
+#This is the main Function, updating code
 if __name__ == "__main__":
     app.run(debug=True, port=8000, host='0.0.0.0')
